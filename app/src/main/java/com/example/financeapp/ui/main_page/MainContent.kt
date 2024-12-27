61f82482ff9bdf82316e41d76f7e2ac2ecfddd8e
Victoriia-Ripka <vikaripka@gmail.com>
2024-12-27 04:34:55 +0100

    main page messages

--- app/src/main/java/com/example/financeapp/ui/main_page/MainContent.kt
+++ app/src/main/java/com/example/financeapp/ui/main_page/MainContent.kt
@@ -1,6 +1,7 @@
 package com.example.financeapp.ui.main_page
 
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,7 +10,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.absoluteOffset
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -41,21 +41,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
 import com.example.financeapp.models.responses.CurrentBalanceCategoriesResponse
 import com.example.financeapp.models.responses.CurrentBalanceResponse
 import com.example.financeapp.services.RetrofitClient
-import com.example.financeapp.ui.Drawer
 import com.example.financeapp.ui.theme.CustomCategoryCard
 import com.example.financeapp.viewmodel.UserViewModel
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -115,7 +112,10 @@ fun MainContent(
                                     currentBalance = it
                                 }
                             } else {
-                                showMessageToUser(response.message())
+                                val jsonObject = JSONObject(response.errorBody()?.string())
+                                val errorMessage = jsonObject.optString("message", "An error occurred")
+                                showMessageToUser(errorMessage)
+                                Log.d("debug", "Main page init failed 1: ${jsonObject}")
                             }
                         }
         
@@ -135,7 +135,10 @@ fun MainContent(
                                     currentBalanceCategories = it
                                 }
                             } else {
-                                showMessageToUser(response.message())
+                                val jsonObject = JSONObject(response.errorBody()?.string())
+                                val errorMessage = jsonObject.optString("message", "An error occurred")
+                                showMessageToUser(errorMessage)
+                                Log.d("debug", "Main page init failed 2: ${jsonObject}")
                             }
                         }
         
@@ -152,12 +155,6 @@ fun MainContent(
                         horizontalAlignment = Alignment.CenterHorizontally
                 )
                 {
-//                    Text(
-//                        text = "Баланс",
-//                        color = MaterialTheme.colorScheme.onSecondary,
-//                        fontSize = 30.sp,
-//                        modifier = Modifier.padding(0.dp, 20.dp)
-//                    )
                     ElevatedCard(
                         elevation = CardDefaults.cardElevation(
                             defaultElevation = 6.dp
@@ -264,7 +261,6 @@ fun MainContent(
             }
         }
     }
-//    Drawer(content)
 
     return content
 
